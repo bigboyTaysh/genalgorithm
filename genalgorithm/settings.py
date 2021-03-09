231f7c075f3ab7e5da9c94f4e853798f14de7aa0
Patryk Wolski <pvtryk.w@gmail.com>
2021-03-09 21:01:35 +0100

    simple post

--- genalgorithm/settings.py
+++ genalgorithm/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'genapp'
 ]
 
 MIDDLEWARE = [
@@ -105,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl-pl'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
--- genapp/views.py
+++ genapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+def index(request):
+    template = loader.get_template('genapp/index.html')
+    context = {
+        'result': "Hello, world. You're at the genapp index."
+    }
+    return HttpResponse(template.render(context, request))
+
+def start(request):
+    rangeA = request.POST['rangeA']
+    rangeB = request.POST['rangeB']
+    template = loader.get_template('genapp/index.html')
+    context = {
+        'rangeA': rangeA,
+        'rangeB': rangeB
+    }
+    return HttpResponse(template.render(context, request))
