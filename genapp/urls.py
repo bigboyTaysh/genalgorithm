ec7e25bdffbd428d5a4d0c605c4c27feead2a752
bigboyTaysh <pvtryk.w@gmail.com>
2021-03-23 17:16:08 +0100

    set view for lab04

--- genapp/urls.py
+++ genapp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('selection/', views.selection, name='selection'),
     path('lab02/', views.lab02, name='lab02'),
     path('lab03/', views.lab03, name='lab03'),
+    path('lab04/', views.lab04, name='lab04'),
 ]
